82cf7bff0184ae49a8d80fa4b39030c66a5aba5a
NedDinev <ned.dinev@gmail.com>
2023-03-20 00:19:59 +0200

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.scss";
 
+import { Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Explore from "./components/Explore/Explore";
+import Create from "./components/Create/Create";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
