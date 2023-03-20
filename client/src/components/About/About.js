cb4edeefd875e694b4f630992e04c6e805db40f0
NedDinev <ned.dinev@gmail.com>
2023-03-20 15:59:47 +0200

    Create about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,5 +1,26 @@
-import React from "react";
+import Card from "react-bootstrap/Card";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <Card>
+      <Card.Body className="m-auto pb-0" style={{ width: "50%" }}>
+        <Card.Title className="text-center mt-4">
+          <h2>About</h2>
+        </Card.Title>
+        <Card.Text className="mb-0 mt-4 pb-0">
+          Welcome to our cooking social network website! Our community is made
+          up of passionate home cooks, professional chefs, and food enthusiasts
+          from all around the world. We believe that food brings people together
+          and we are dedicated to creating a platform that allows our members to
+          connect, share, and inspire one another through their love of cooking.
+        </Card.Text>
+      </Card.Body>
+      <Card.Img
+        className="m-auto"
+        style={{ width: "50%", alignItems: "center" }}
+        variant="bottom"
+        src="/assets/images/illustrations/chef-panels.jpg"
+      />
+    </Card>
+  );
 }
