d84892836e77fc81d0ce46a13798dfbca8295314
NedDinev <ned.dinev@gmail.com>
2023-03-21 17:17:19 +0200

    Add recipe cards to explore page

--- client/src/components/Explore/Explore.js
+++ client/src/components/Explore/Explore.js
@@ -1,5 +1,16 @@
-import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import RecipeCard from "./RecipeCard/RecipeCard";
 
 export default function Explore() {
-  return <div>Explore</div>;
+  return (
+    <Row xs={1} sm={1} md={2} lg={3} className="g-3 mx-5">
+      {Array.from({ length: 6 }).map((_, idx) => (
+        <Col>
+          <RecipeCard />
+        </Col>
+      ))}
+    </Row>
+  );
 }
