af2b22be3a578d647e847f722c5669d428a03a4b
NedDinev <ned.dinev@gmail.com>
2023-03-23 00:55:23 +0200

    Add register form validation

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,40 +1,110 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+import FormInput from "./FormInput/FormInput";
+
 export default function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      console.log(values);
+      //TODO: register, login and redirect to explore page
+    }
+
+    setValidated(true);
+  };
+
+  const inputs = [
+    {
+      id: 1,
+      controlId: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and should not include special characters",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      controlId: "email",
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+      errorMessage: "It should be a valid email address",
+      required: true,
+    },
+    {
+      id: 3,
+      controlId: "password",
+      name: "password",
+      type: "text",
+      placeholder: "Password",
+      label: "Password",
+      errorMessage:
+        "Password must contain minimum eight characters, one number and one special character:",
+      pattern: "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+      required: true,
+    },
+    {
+      id: 4,
+      controlId: "confirmPassword",
+      name: "confirmPassword",
+      type: "text",
+      placeholder: "Confirm Password",
+      label: "Confirm Password",
+      errorMessage: "Passwords don't match",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
     <Card className="container border-0 ">
       <Card.Body className="m-auto pb-0" style={{ width: "50%" }}>
         <Card.Title className="text-center mt-4">
           <h2>Register</h2>
         </Card.Title>
-        <Card.Text className="mb-0 mt-4 pb-0">
-          <Form>
-            <Form.Group className="mb-3" controlId="username">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
 
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="confirmPassword">
-              <Form.Label>Confirm password</Form.Label>
-              <Form.Control type="password" placeholder="Repeat password" />
-            </Form.Group>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          {inputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          ))}
 
-            <Button className="mb-3" variant="success" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Text>
+          <Button className="mb-3" variant="success" type="submit">
+            Submit
+          </Button>
+        </Form>
+
         <Card.Text>
           You already have an account? <Link to="/login">Login</Link>
         </Card.Text>
