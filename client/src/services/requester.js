c0559acbd2df92a81eaf5d03c01e1b0928d89307
NedDinev <ned.dinev@gmail.com>
2023-03-25 17:13:21 +0200

    Handle response

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -1,31 +1,37 @@
 const request = async (method, url, data) => {
-    const options = {};
-    
-    if (method !== 'GET') {
-        options.method = method;
+  const options = {};
 
-        if (data) {
-            options.headers = {
-                'content-type': 'application/json',
-            };
+  if (method !== "GET") {
+    options.method = method;
 
-            options.body = JSON.stringify(data);
-        }
+    if (data) {
+      options.headers = {
+        "content-type": "application/json",
+      };
+
+      options.body = JSON.stringify(data);
     }
+  }
 
-    const response = await fetch(url, options);
+  const response = await fetch(url, options);
 
-    try {
-        const result = await response.json();
+  if (!response.ok) {
+    const result = await response.json();
 
-        return result;
-    } catch (error) {
-        return {};
-    }
+    throw result;
+  }
+
+  if (response.status === 204) {
+    return {};
+  }
+
+  const result = await response.json();
+
+  return result;
 };
 
-export const get = request.bind(null, 'GET');
-export const post = request.bind(null, 'POST');
-export const put = request.bind(null, 'PUT');
-export const patch = request.bind(null, 'PATCH');
-export const del = request.bind(null, 'DELETE');
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const patch = request.bind(null, "PATCH");
+export const del = request.bind(null, "DELETE");
