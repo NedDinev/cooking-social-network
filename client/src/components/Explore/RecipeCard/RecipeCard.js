172b9b744a223fdc25e562c2cc567cb6cbd93446
NedDinev <ned.dinev@gmail.com>
2023-04-06 18:47:04 +0300

    Add shadows to cards

--- client/src/components/Explore/RecipeCard/RecipeCard.js
+++ client/src/components/Explore/RecipeCard/RecipeCard.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function RecipeCard({ recipe }) {
   return (
-    <Card className="m-5">
+    <Card className="m-5 shadow p-3 mb-5 bg-white rounded">
       <Card.Img className="card-img-top" variant="top" src={recipe.imageUrl} />
       <Card.Body>
         <Card.Title>{`${recipe.recipeName}`}</Card.Title>
         <Card.Text>
-          Type: {recipe.dishType} Cooking time: {recipe.cookingTime}
+          Type: {recipe.dishType}
           <br />
           Difficulty: {recipe.difficulty}
         </Card.Text>
