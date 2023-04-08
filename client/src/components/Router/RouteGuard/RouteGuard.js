eb3a0c7e095c17a8e45f8ae454af253644f276a1
NedDinev <ned.dinev@gmail.com>
2023-04-08 18:55:52 +0300

    Add router guard

---
+++ client/src/components/Router/RouteGuard/RouteGuard.js
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function RouteGuard({ children }) {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  if (!isAuthenticated) return <Navigate to="/login" />;
+
+  return <>{children}</>;
+}
--- client/src/components/Router/Router.js
+++ client/src/components/Router/Router.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "../Home/Home";
 import Explore from "../Explore/Explore";
@@ -11,21 +11,62 @@ import Details from "../Details/Details";
 import Delete from "../Delete/Delete";
 import Edit from "../Edit/Edit";
 import Search from "../Search/Search";
+import RouteGuard from "./RouteGuard/RouteGuard";
+import Profile from "../Profile/Profile";
+import InvalidPath from "../InvalidPath/InvalidPath";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/explore" element={<Explore />} />
-      <Route path="/create" element={<Create />} />
+      <Route
+        path="/create"
+        element={
+          <RouteGuard>
+            <Create />
+          </RouteGuard>
+        }
+      />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/logout" element={<Logout />} />
+      <Route
+        path="/logout"
+        element={
+          <RouteGuard>
+            <Logout />
+          </RouteGuard>
+        }
+      />
+      <Route
+        path="/profile/:userId"
+        element={
+          <RouteGuard>
+            <Profile />
+          </RouteGuard>
+        }
+      />
       <Route path="/about" element={<About />} />
       <Route path="/details/:recipeId" element={<Details />} />
-      <Route path="/details/:recipeId/edit" element={<Edit />} />
-      <Route path="/details/:recipeId/delete" element={<Delete />} />
+      <Route
+        path="/details/:recipeId/edit"
+        element={
+          <RouteGuard>
+            <Edit />
+          </RouteGuard>
+        }
+      />
+      <Route
+        path="/details/:recipeId/delete"
+        element={
+          <RouteGuard>
+            <Delete />
+          </RouteGuard>
+        }
+      />
       <Route path="/search/:recipeName" element={<Search />} />
+      {/* for invalid path */}
+      <Route path="*" element={<InvalidPath />} />
     </Routes>
   );
 }
